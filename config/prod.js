2b0c6a32d9d1b6adc1a4b5ddc38d4a9caf3bac09
Aadesh Goyal <goyalaadesh462@gmail.com>
2020-10-02 15:17:47 +0530

    fourth commit

--- config/prod.js
+++ config/prod.js
@@ -3,4 +3,5 @@ module.exports = {
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     databaseURLString: process.env.DATABASE_URL_STRING,
     cookieKey: process.env.COOKIE_KEY,
+    googleCallBackUrl: process.env.GOOGLE_CALL_BACK_URL
 }
