cccb816c9ee9a13e0dd0162aaecc6a6db391ee51
Aadesh Goyal <goyalaadesh462@gmail.com>
2020-10-02 15:48:37 +0530

    refactoring

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 const mongoose = require('mongoose');
 const KEYS = require('./config/keys');
+const process = require('process');
 const app = require('./app');
 const User = require('./models/user');
 const { use } = require('passport');
  
 async function start() {
+
+    if(process.env.NODE_ENV === 'production') {
+        if(!process.env.GOOGLE_CLIENT_ID) {
+            console.log('Please provide google client id');
+            process.exit(1);
+        }
+
+        if(!process.env.GOOGLE_CLIENT_SECRET) {
+            console.log('Please provide google client id');
+            process.exit(1);
+        }
+
+        if(!process.env.DATABASE_URL_STRING) {
+            console.log('Please provide google client id');
+            process.exit(1);
+        }
+
+        if(!process.env.COOKIE_KEY) {
+            console.log('Please provide google client id');
+            process.exit(1);
+        }
+
+        if(!process.env.GOOGLE_CALL_BACK_URL) {
+            console.log('Please provide google client id');
+            process.exit(1);
+        }
+    }
+
     try {
         await mongoose.connect(KEYS.databaseURLString,{
             useCreateIndex: true,
